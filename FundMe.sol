8d422dd77b04ef78a212e1dd95af9e169dc937d7
acidrums7 <edoardo.carradori.96@gmail.com>
2023-11-11 16:36:13 +0100

    Getting real world price data (Chainlink)

--- FundMe.sol
+++ FundMe.sol
@@ -6,13 +6,12 @@
 pragma solidity ^0.8.23;
 
 contract FundMe {
-    uint256 public myValue = 1;
+    uint256 public minimumUsd = 5;
 
     function fund() public payable {
         // Allow to send $
         // Have a minimum $ send
         // 1) How to send ETH to this contract?
-        myValue += 2;
         require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 Wei = 1 ETH = 1 00000 00000 00000 000 (18 zeros)
         // What is a revert?
         // Undo any actions that have been done, and send the remaing gas back
