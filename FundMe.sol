820d8d252c52f541d6db7f911b621742de163ced
acidrums7 <edoardo.carradori.96@gmail.com>
2023-11-11 16:01:34 +0100

    Sending ETH through a function

---
+++ FundMe.sol
@@ -0,0 +1,17 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+contract FundMe {
+    function fund() public payable {
+        // Allow to send $
+        // Have a minimum $ send
+        // 1) How to send ETH to this contract?
+        require (msg.value > 1e18); // 1e18 = 1 ETH = 1 00000 00000 00000 000 (18 zeros)
+    }
+
+    // function withdraw() public {}
+}
