1b1b7e327f2a7e2235c4791b197f47d0fdbe8430
acidrums7 <edoardo.carradori.96@gmail.com>
2023-11-11 16:07:58 +0100

    Reverts

--- FundMe.sol
+++ FundMe.sol
@@ -6,11 +6,18 @@
 pragma solidity ^0.8.23;
 
 contract FundMe {
+    uint256 public myValue = 1;
+
     function fund() public payable {
         // Allow to send $
         // Have a minimum $ send
         // 1) How to send ETH to this contract?
-        require (msg.value > 1e18, "didn't send enough ETH"); // 1e18 Wei = 1 ETH = 1 00000 00000 00000 000 (18 zeros)
+        myValue += 2;
+        require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 Wei = 1 ETH = 1 00000 00000 00000 000 (18 zeros)
+        // What is a revert?
+        // Undo any actions that have been done, and send the remaing gas back
+
+        //... a ton of computation
     }
 
     // function withdraw() public {}
